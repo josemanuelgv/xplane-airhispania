456861c30d504e8222571f57be0ebad0fc222d38
josemanuelgv@gmail.com <josemanuelgv@gmail.com@eb9c1e43-f132-4ab6-8761-333b0f37910b>
2014-07-22 19:57:11 +0000

    Obsoleto

--- x-ivap-ahs/src/Linux/install.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-os=`uname`
-if [ $os == "Darwin" ] ; then
-	cp -v Release/Resources/plugins/X-IvAp.xpl /X-Plane\ 9.00\ Beta-11/Resources/plugins/X-IvAp.xpl/Contents/MacOS/X-IvAp 
-else
-	cp -v Release/Resources/plugins/X-IvAp.xpl ~/XPlane/Resources/plugins
-fi
