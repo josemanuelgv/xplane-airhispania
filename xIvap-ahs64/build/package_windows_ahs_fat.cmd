34131a249e866bcef3301c5ddbf1f26c3251bea4
josemanuelgv@gmail.com <josemanuelgv@gmail.com@eb9c1e43-f132-4ab6-8761-333b0f37910b>
2014-09-08 22:41:06 +0000

    Script para crear builds de tipo FAT plugin Incluidas las dll's necesarias para win32

---
+++ xIvap-ahs64/build/package_windows_ahs_fat.cmd
@@ -0,0 +1,40 @@
+SET VERSION=030_AHSr112
+SET ROOT=X-IvAp_%VERSION%
+SET FOLDER=X-IvAp AHS
+SET RESFOLDER=X-IvAp AHS Resources
+
+mkdir %ROOT%
+
+REM  Core README
+xcopy ..\doc\*.txt %ROOT%
+
+REM  Introduction
+mkdir %ROOT%\introduction
+copy ..\doc\*.htm %ROOT%\introduction
+copy ..\doc\*.gif %ROOT%\introduction
+
+REM  Program
+mkdir "%ROOT%\for_plugins_folder\%FOLDER%"
+mkdir "%ROOT%\for_plugins_folder\%FOLDER%\32"
+mkdir "%ROOT%\for_plugins_folder\%FOLDER%\64"
+
+REM 64bit version
+copy "%XPlaneDir%\Resources\Plugins\%FOLDER%\64\win.xpl" "%ROOT%\for_plugins_folder\%FOLDER%\64"
+copy ..\src\FMOD\FMODWindows\api\fmodex64.dll "%ROOT%\for_plugins_folder\%FOLDER%\64"
+copy .\msvc_64\msvcp110.dll "%ROOT%\for_plugins_folder\%FOLDER%\64"
+copy .\msvc_64\msvcr110.dll "%ROOT%\for_plugins_folder\%FOLDER%\64"
+
+REM 32bit version
+copy "%XPlaneDir%\Resources\Plugins\%FOLDER%\32\win.xpl" "%ROOT%\for_plugins_folder\%FOLDER%\32"
+copy ..\src\FMOD\FMODWindows\api\fmodex.dll "%ROOT%\for_plugins_folder\%FOLDER%\32"
+copy .\msvc_32\msvcp110.dll "%ROOT%\for_plugins_folder\%FOLDER%\32"
+copy .\msvc_32\msvcr110.dll "%ROOT%\for_plugins_folder\%FOLDER%\32"
+
+
+mkdir "%ROOT%\for_plugins_folder\%RESFOLDER%\"
+xcopy /E "X-IvAp Resources" "%ROOT%\for_plugins_folder\%RESFOLDER%\"
+
+REM Compress
+del /s /q xivap_*.zip
+7za.exe a xivap_%VERSION%.zip %ROOT%
+del /s /q %ROOT%
